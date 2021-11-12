2cefdb4a374af3e4693e81410fe51da26741baa4
tektechnologies <info@tektechnologies.com>
2021-11-12 17:58:42 -0600

    add images rendering from for loop

--- js/app.js
+++ js/app.js
@@ -1,68 +1,68 @@
-'use strict';
-console.log('app.js is connected.');
+// 'use strict';
+// console.log('app.js is connected.');
 
 
-let userName = prompt('What is your name?');
-let confirmMessage = confirm('Do you want to continue?');
-let hourNow = prompt('What time is it, in Military time?');
-let greeting;
+// let userName = prompt('What is your name?');
+// let confirmMessage = confirm('Do you want to continue?');
+// let hourNow = prompt('What time is it, in Military time?');
+// let greeting;
 
-if(hourNow > 18 && hourNow < 24){
-    greeting = 'Good Evening';
-} else if(hourNow >12 && hourNow < 18){
-    greeting = 'Good Afternoon';
-}else if(hourNow > 6 && hourNow <12){
-    greeting = 'Good Morning!, Get some OJ!';
-} else {
-    greeting = 'Why arent you in bed!!!! go to sleep';
-}
-console.log('this is the greeting',greeting);
-document.write('Welcome ' + userName + ' ' + greeting);
-
-
-//This section below was the if/else logic after we added the Date object and the getHours() method. This was part of Class 6 Demo, to show objects and methods.
-/** 
-let today = new Date();
-console.log('this is the letiable today',today);
-let hourNow = today.getHours(); 
-let greeting;
-if (hourNow > 18 && hourNow < 24) {
-  greeting = 'Good evening, Class!';
-} else if (hourNow > 12 && hourNow < 24) {
-  greeting = 'Good afternoon, Class!';
-} else if (hourNow >= 0 && hourNow < 24) {
-  greeting = 'Good morning!';
-} else {
-  greeting = 'Welcome!';
-}
-document.write('<h3>' + greeting + '</h3>');
-*/
+// if(hourNow > 18 && hourNow < 24){
+//     greeting = 'Good Evening';
+// } else if(hourNow >12 && hourNow < 18){
+//     greeting = 'Good Afternoon';
+// }else if(hourNow > 6 && hourNow <12){
+//     greeting = 'Good Morning!, Get some OJ!';
+// } else {
+//     greeting = 'Why arent you in bed!!!! go to sleep';
+// }
+// console.log('this is the greeting',greeting);
+// document.write('Welcome ' + userName + ' ' + greeting);
 
 
-
-function makeSandwich(){
-  //show first to see the undefined without return. 
-  console.log('we have invoked our function in order to run our console log');
-  // return 'hello from the js file';
-}
+// //This section below was the if/else logic after we added the Date object and the getHours() method. This was part of Class 6 Demo, to show objects and methods.
+// /** 
+// let today = new Date();
+// console.log('this is the letiable today',today);
+// let hourNow = today.getHours(); 
+// let greeting;
+// if (hourNow > 18 && hourNow < 24) {
+//   greeting = 'Good evening, Class!';
+// } else if (hourNow > 12 && hourNow < 24) {
+//   greeting = 'Good afternoon, Class!';
+// } else if (hourNow >= 0 && hourNow < 24) {
+//   greeting = 'Good morning!';
+// } else {
+//   greeting = 'Welcome!';
+// }
+// document.write('<h3>' + greeting + '</h3>');
+// */
 
 
 
-
-/** 
-JavaScript - Coding language of the web. Used to create and control dynamic website content.
-
-conditionals - A series of checks that determine an outcome. If something happens, the do this. If that does not happen but this happens, do this. If all else fails, then do this last thing.
-
-operators - Series of symbols that perform arithmetic on numbers. (+, -, *, **, /, %. ++, --)
+// function makeSandwich(){
+//   //show first to see the undefined without return. 
+//   console.log('we have invoked our function in order to run our console log');
+//   // return 'hello from the js file';
+// }
 
 
-data types - A particular kind of data item, integer(number), string (words), boolean (true/false), array. 
-There are more advanced data types that will be introduced in 401.
 
 
-variable - a representation or nick name for some type of data.
+// /** 
+// JavaScript - Coding language of the web. Used to create and control dynamic website content.
 
-*/
+// conditionals - A series of checks that determine an outcome. If something happens, the do this. If that does not happen but this happens, do this. If all else fails, then do this last thing.
+
+// operators - Series of symbols that perform arithmetic on numbers. (+, -, *, **, /, %. ++, --)
+
+
+// data types - A particular kind of data item, integer(number), string (words), boolean (true/false), array. 
+// There are more advanced data types that will be introduced in 401.
+
+
+// variable - a representation or nick name for some type of data.
+
+// */
 
 
